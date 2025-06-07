20874b326ae8006e0aee46177ce2c9386887e8ef
Sax300-cpu <keander300@gmail.com>
2025-06-07 12:32:05 -0500

    Docker Inicializacion

--- Sistema-Gestion-Seguros/backend/app.py
+++ Sistema-Gestion-Seguros/backend/app.py
@@ -21,6 +21,10 @@ app = Flask(
     template_folder="templates"
 )
 app.secret_key = os.getenv("SECRET_KEY")
+app.config['MYSQL_HOST']     = os.getenv('DB_HOST', 'db')
+app.config['MYSQL_USER']     = os.getenv('DB_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD')
+app.config['MYSQL_DB']       = os.getenv('DB_NAME')
 mysql = init_db(app)
 
 # ===================================
